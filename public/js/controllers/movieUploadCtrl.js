e52f9a45f491ca5a0fbe1019e75115ff388bcfab
Alex Osman <aro39@drexel.edu>
2016-06-03 10:19:28 -0400

    server cleaning

--- public/js/controllers/movieUploadCtrl.js
+++ public/js/controllers/movieUploadCtrl.js
@@ -2,7 +2,15 @@ angular
 	.module("cosmosCloud")
 	.controller("movieUploadCtrl", ["$scope", '$http', 'FileUploader', function($scope, $http, FileUploader) {
 		$scope.message = "movieUploadCtrl"
+		$scope.toSQL = [];
 		$scope.uploader = new FileUploader({
-			url: '/uploadFile'
+			url: '/uploadMovie',
+			onAfterAddingAll: function(addedItems) {
+				for (var i = 0; i < addedItems.length; i++) {
+					var sqlEntry = {};
+					sqlEntry.title = prompt("Please enter the title", addedItems[i].file.name);
+					
+				}
+			}
 		});
 	}])
