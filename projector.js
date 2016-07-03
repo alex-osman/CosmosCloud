75980bd252166a529c0be5bfe5f6913b3f7e63e9
Alex <alexosman@live.com>
2016-07-03 19:11:41 -0400

    projector export

--- projector.js
+++ projector.js
@@ -1,5 +1,41 @@
 function Projector(ip) {
-	console.log("my ip is " + ip)
+	this.ip = ip;
+	this.port = 1337;
+	console.log("Projector " + ip + " initialized")
 }
-console.log("inside")
+
+Projector.prototype = {
+	getIp: function() {
+		return this.ip;
+	},
+	setIp: function(ip) {
+		this.ip = ip;
+	}
+	command: function(command) {
+		http.get({host: this.ip,port: this.port,path: "/api/command/" + command}, function(data) {
+			return data;
+		})
+	},
+	stream: function(stream) {
+		http.post({host: this.ip, port: this.port, path: "/api/stream"}, {url: stream}, function(data) {
+			return data;
+		})
+	},
+	music: function(song) {
+		http.get({host: this.ip,port: this.port,path: "/api/music/" + song}, function(data) {
+			return data;
+		})	
+	},
+	movie: function(movie) {
+		http.get({host: this.ip,port: this.port,path: "/api/movie/" + movie}, function(data) {
+			return data;
+		})
+	},
+	status: function(movie) {
+		http.get({host: this.ip, port: this.port, path: "/api/status"}, function(data) {
+			return data;
+		})
+	}
+}
+
 module.exports = Projector;
