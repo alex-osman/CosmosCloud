d75fea413bc769cb715e610958b0b8dd35c46ca5
Alex Osman <aro39@drexel.edu>
2016-09-15 01:12:36 -0400

    test and shairport merged

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
 var express = require('express');
-var Shairport = require('./shairport.js')
+function Shairport() {
+	this.artist = "none";
+	this.title = "nothing";
+	this.album = "nada";
+}
+Shairport.prototype.getData = function(callback) {
+	var fs = require('fs')
+	fs.readFile("/tmp/metadata.txt", function(err, dataBuffer) {
+		var data =  dataBuffer.toString('ascii');
+		//Metadata
+		data = data.substring(data.lastIndexOf('Album Name'));
+		data = data.substring(data.indexOf('"')+1);
+		//album Name: "To All My Friends, Blood Makes The Blade Holy".
+		this.album = data.substring(0, data.indexOf('"'));
+		data = data.substring(data.indexOf('Artist'));
+		data = data.substring(data.indexOf('"')+1)
+		this.artist = data.substring(0, data.indexOf('"'));
+		data = data.substring(data.indexOf('Title: "'));
+		data = data.substring(data.indexOf('"') + 1);
+		this.title = data.substring(0, data.indexOf('"'));
+		callback();
+	})
+}
 var shairport = new Shairport();
-shairport.getData(function() {
-	console.log("Song: " + this.title)
-	console.log("By: " + this.artist)
-	console.log(this.album)
-});
 var app = express();
 
 app.get('/metadata', function(req, res) {
