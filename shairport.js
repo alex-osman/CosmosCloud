f82fc2f65543e402345ce710376ef0c7328d914c
Alex Osman <aro39@drexel.edu>
2016-09-15 00:59:15 -0400

    shairport implemented

--- shairport.js
+++ shairport.js
@@ -1,13 +1,24 @@
-Shairport = {
-	Album: "",
-	Artist: "",
-	Title: "",
-	start: function() {
-		console.log("Shairport")
-		var fs = require('fs')
-		fs.readFile("/tmp/metadata", function(err, data) {
-			console.log(data.toString('ascii'));
-		})
-	}
+function Shairport() {
+	this.artist = "none";
+	this.title = "nothing";
+	this.album = "nada";
+}
+Shairport.prototype.getData = function(callback) {
+	var fs = require('fs')
+	fs.readFile("/tmp/metadata.txt", function(err, dataBuffer) {
+		var data =  dataBuffer.toString('ascii');
+		//Metadata
+		data = data.substring(data.lastIndexOf('Album Name'));
+		data = data.substring(data.indexOf('"')+1);
+		//album Name: "To All My Friends, Blood Makes The Blade Holy".
+		this.album = data.substring(0, data.indexOf('"'));
+		data = data.substring(data.indexOf('Artist'));
+		data = data.substring(data.indexOf('"')+1)
+		this.artist = data.substring(0, data.indexOf('"'));
+		data = data.substring(data.indexOf('Title: "'));
+		data = data.substring(data.indexOf('"') + 1);
+		this.title = data.substring(0, data.indexOf('"'));
+		callback();
+	})
 }
 module.exports = Shairport;
