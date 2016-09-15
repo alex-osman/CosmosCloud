c6cbc42573b41167f3af05c2c564d7c15ecbb276
Alex Osman <aro39@drexel.edu>
2016-09-15 01:07:00 -0400

    test should be a server for shairport-running device

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var Shairport = require('./shairport.js')
 var shairport = new Shairport();
 shairport.getData(function() {
@@ -5,3 +6,7 @@ shairport.getData(function() {
 	console.log("By: " + this.artist)
 	console.log(this.album)
 });
+var app = express();
+
+app.get('/metadata')
+app.listen(8081, '0.0.0.0');
