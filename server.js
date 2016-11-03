da803326d431ad2a049d4a089100d78fc6b49ebc
Alex Osman <aro39@drexel.edu>
2016-11-03 00:30:05 -0400

    renamed coreserver to server

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 var express	= require("express");
-var http	= require('http')
 var bodyParser = require('body-parser')
 var app		= express();
-app.use(bodyParser.json({limit: '5000mb'}))
-app.use(bodyParser.urlencoded({limit: '5000mb'}))
+var ping = require('ping');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var fs = require('fs');
-var util = require('util'),
-			spawn = require('child_process').spawn,
-			exec = require('child_process').exec,
-			child;
+var http = require('http');
+var	exec = require('child_process').exec;
 var mysql = require("mysql");
+
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
@@ -19,252 +16,246 @@ var connection = mysql.createConnection({
 	database: 'cosmos'
 });
 connection.connect()
-var movie_folder = "public/assets/movies/"
-var picture_folder = "public/assets/pictures/"
-var doc_folder = "public/assets/docs/"
-var music_folder = "public/assets/music/"
-
-var omxplayer = null;
-
-
-
-var util = require('util'),
-	exec = require('child_process').exec,
-	child;
-
 app.use(express.static(__dirname + "/public"));
 
+/*THEATRE*/
+/*THEATRE INITIALIZATION*/
+var theatres = require('./theatre.js')
+theatres.add('10.0.0.12', 1337, 'Bedroom');
+theatres.add('10.0.0.88', 1337, 'LivingRoom');
 
-/* UPLOAD MOVIE */
-app.post('/uploadMovie', multipartMiddleware, function(req, res) {
-	var tmp_path = req.files.file.path;
-	console.log(tmp_path);
-	var target_path = movie_folder + req.files.file.originalFilename;
-	console.log(target_path)
-	var src = fs.createReadStream(tmp_path);
-	var dest = fs.createWriteStream(target_path)
-	src.pipe(dest);
-	res.send("ok")
-});
-
-/* UPLOAD PICTURE */
-app.post('/uploadPicture', multipartMiddleware, function(req, res) {
-	if (req.files.file.originalFilename === "image.jpg")
-		req.files.file.originalFilename = "image" + (new Date).getTime() + ".jpg"
-	var tmp_path = req.files.file.path;
-	console.log(tmp_path);
-	var target_path = picture_folder + req.files.file.originalFilename;
-	console.log(target_path)
-	var src = fs.createReadStream(tmp_path);
-	var dest = fs.createWriteStream(target_path)
-	src.pipe(dest);
-	res.send("ok")
-});
-
-app.post('/uploadDoc', multipartMiddleware, function(req, res) {
-	var tmp_path = req.files.file.path;
-	console.log(tmp_path);
-	var target_path = doc_folder + req.files.file.originalFilename;
-	console.log(target_path)
-	var src = fs.createReadStream(tmp_path);
-	var dest = fs.createWriteStream(target_path)
-	src.pipe(dest);
-	res.send("ok")
-});
-
-
-app.get('/api/movies', function(req, res) {
-	var movie_path = "assets/movies/"
-	var dir = fs.readdirSync(movie_folder)
-	var movies = [];
-	var counter = 0;
-	dir.forEach(function(value) {
-		fs.stat(movie_folder + value, function(err, data) {
-			movies.push({
-				name: value,
-				path: movie_path + value,
-				bytes: data.size
-			});
-			counter++;
-			if(counter === dir.length)
-				res.send(movies)
+//Encapsulate get requests to a theatre
+//TODO: Move to Theatre.js module
+var getRequest = function(url, num, callback) {
+	http.get(theatres.getOptions(url, num), function(response) {
+		var body = "";
+		response.on('data', function(chunk) {
+			body += chunk;
 		});
-	});
+		response.on('end', function() {
+			callback(body);
+		});
+	}).on('error', function(err) {
+		if (err.code == 'ECONNREFUSED' | err.code == 'EHOSTUNREACH') {
+			callback(err.code);
+		} else throw err;
+	})
+}
+
+/*THEATRE METHODS*/
+app.get('/getTheatres', function(req, res) {
+	res.send(theatres.names)
 })
 
-app.get('/api/SQL/movies', function(req, res) {
-	connection.query('SELECT * from movies;', function(err, rows, field) {
+app.get('/dbus/:num/:type/:command', function(req, res) {
+	console.log('/dbus/' + req.params.type + '/' + req.params.command);
+	getRequest('/dbus/' + req.params.type + '/' + req.params.command, req.params.num, function(str) {
+		console.log(str);
+		res.send(str);
+	})
+})
+
+/********************************************************
+ *
+ *	This route is to be used by the RSSI tracker
+ *	Transitions your theatre asset from 'source' to 'target'
+ *
+ *	Ex: /transition/0/1
+ *			Simpsons is playing in living room
+ *			Get asset
+ *			Get position
+ *			Shut down living room theatre
+ *			Play asset at position in bedroom theatre
+ *
+ ********************************************************/
+app.get('/transition/:source/:target', function(req, res) {
+	var source = parseInt(req.params.source);
+	var target = parseInt(req.params.target);
+	console.log("Moving video from " + source + " to " + target);
+	//First get the asset from source
+	getRequest('/dbus/player/GetSource', source, function(videoSrc) {
+		console.log("1: " + videoSrc)
+		//Get the position from source
+		getRequest('/dbus/prop/Position', source, function(pos) {
+			//Kill the source
+			getRequest('/dbus/action/15', source, function(){})
+			//Play the asset on target
+			console.log(pos);
+			var position = pos.split(' ')[1].trim()
+			console.log("2: " + position)
+			getRequest('/play/stream/' + encodeURIComponent(videoSrc), target, function(response){
+				console.log("3: " + response)
+
+				//Set the position on target
+				setTimeout(function() {
+					getRequest('/dbus/player/GetSource', target, function(str) {
+						console.log("target is playing: " + str);
+						
+
+						console.log("Setting position")
+						getRequest('/dbus/setPosition/' + position, target, function(str) {
+							console.log("Got: " + str)
+							res.send(str);
+						})					
+					})
+				}, 3000) 
+				//Need to allow 3 seconds for the process to start
+				//How to start at a given position. Dbus is for inter-process commmunication, not on start
+			})
+		})
+	})
+})
+
+/*THEATRE LIVE-STREAMS*/
+var streams = [];
+
+app.get('/sql/livestreams', function(req, res) {
+	connection.query('SELECT * FROM Streams', function(err, rows, fields) {
+		streams = rows;
 		if (err) throw err;
 		res.send(rows);
 	})
 })
 
-app.get('/api/pictures', function(req, res) {
-	var dir = fs.readdirSync(picture_folder)
-	res.send(dir)
-})
-
-app.get('/api/music', function(req, res) {
-	var dir = fs.readdirSync(music_folder)
-	res.send(dir)
-})
-
-app.get('/api/docs', function(req, res) {
-	var dir = fs.readdirSync(doc_folder)
-	res.send(dir)
-})
-
-app.get('/SQL', function(req, res) {
-	connection.query('SELECT * from ledger;', function(err, rows, field) {
+/*THEATRE MOVIES*/
+app.get('/sql/movies', function(req, res) {
+	connection.query('SELECT * FROM movies', function(err, rows, fields) {
 		if (err) throw err;
-		res.send(rows)
+		res.send(rows);
 	})
 })
 
-app.post('/SQL/Transactions', function(req, res) {
-	var post = req.body;
-	connection.query('INSERT INTO ledger SET ?', post, function(err, result) {
-		console.log(err)
-		console.log(result)
-		res.send("ok");
+app.get('/play/:num/stream/:id', function(req, res) {
+	streams.forEach(function(stream) {
+		if (stream.id == req.params.id) {
+			getRequest('/play/stream/' + encodeURIComponent(stream.url), parseInt(req.params.num), function(str) {
+				console.log(str);
+				res.send(str);
+			})
+		}
 	})
 })
 
-app.post('/SQL/insert/movie', function(req, res) {
-	var post = req.body;
-	connection.query('INSERT INTO movies SET ?', post, function(err, result) {
-		console.log(err)
-		console.log(result)
-		res.send("ok!")
+app.get('/play/:num/:url', function(req, res) {
+	getRequest('/play/movie/' + req.params.url, parseInt(req.params.num), function(str) {
+		console.log(str);
+		res.send(str);
 	})
 })
 
-app.get('/SQL/remove/:id', function(req, res) {
-	connection.query('DELETE from ledger where ID=' + req.params.id +';', function(err, result) {
-		console.log(err)
-		console.log(result)
-		res.send("ok")
+/*END THEATRE*/
+
+
+
+
+
+/*LEDGER MODULE*/
+app.get("/ledger/SQL", function(req, res) {
+	connection.query('SELECT * FROM ledger', function(err, rows, fields) {
+		if (err)
+			throw err;
+		res.send(rows);
 	})
 })
 
-app.get('/smarthome/toggle', function(req, res) {
-	http.get({host:'10.0.0.64', port: 8080, path:'/toggle0'}, function(data) {
-		res.send("okay!")
-	})
+/*END LEDGER*/
+
+
+
+/*USERS*/
+var users = [];
+
+connection.query('SELECT * FROM Users', function(err, rows, fields) {
+	users = rows;
+	pingUsers();
 })
 
-app.get('/smarthome/turnOff', function(req, res) {
-	http.get({host:'10.0.0.64', port: 8080, path:'/turnOff0'}, function(data) {
-		res.send("okay!")
+//Pings every user in users to see if Alive or not.  Records the last time they were alive
+var pingUsers = function() {
+	users.forEach(function(host) {
+		if (host.ping == 1) {
+			ping.sys.probe(host.IP, function(isAlive) {
+				host.isAlive = isAlive;
+				if (isAlive) {
+					connection.query("UPDATE Users SET time='" + new Date().getTime() + "' WHERE id='" + host.id + "';", function(err, rows, fields) {
+						console.log("Added time to " + host.Name)
+					})
+				}
+			})
+		}
 	})
-})
-
-app.get('/smarthome/turnOn', function(req, res) {
-	http.get({host:'10.0.0.64', port: 8080, path:'/turnOn0'}, function(data) {
-		res.send("okay!")
-	})
-})
-
-var killOmxplayer = function() {
-	exec('pkill -f omxplayer');
+	//Ping users every 30 seconds
+	setTimeout(pingUsers, 30000);
 }
 
-var startOmxplayer = function(url) {
-	killOmxplayer();
-	fs.writeFile('FIFO', '', function(err) {if (err) throw err;})
-	omxplayer = exec('omxplayer -b -o local ' + url + ' < FIFO', function(er, out, err) {
-		http.get({host:'10.0.0.64', port: 8080, path:'/turnOn0'}, function(data) {})
-	});
-}
-
-
-
-//~~~~STREAM~SONG~MUSIC~~~~
-app.post('/api/remote/stream', function(req, res) {
-	var stream = req.body.url
-	console.log("Starting stream: " + stream);
-	startOmxplayer("'" + stream + "'")
-	res.send("ok");
+app.get('/users', function(req, res) {
+	connection.query('SELECT * FROM Users', function(err, rows, fields) {
+		users = rows;
+		res.send(users);
+		console.log("sent users to client");
+	})	
 })
-
-app.get('/api/remote/music/:song', function(req, res) {
-	var song = req.params.song
-	console.log("Starting up song: " + song);
-	startOmxplayer(music_folder + song.replace(/ /g, '\\ '))
-	res.send("okay")
-})
-
-app.get('/api/remote/open/:movie', function(req, res) {
-	http.get({host:'10.0.0.64', port: 8080, path:'/turnOff0'}, function(data) {})	
-	var movie = req.params.movie;
-	console.log("Starting up: " + movie);
-	startOmxplayer(movie_folder + movie)
-	res.send("okay");
-})
-
-app.get('/api/remote/twitch/:user', function(req, res) {
-	var user = req.params.user;
-	exec('twitchurl ' + user, function(stdin, stdout, stderr) {
-		console.log(stdout)
-		var arr = stdout.split('\n');
-		var url = arr[4].substring(arr[4].indexOf('http'));
-		startOmxplayer("'" + url + "'")
-		res.send("okay " + user);
-	})
-})
-
-app.get('/api/remote/:command', function(req, res) {
-	var command = req.params.command;
-	switch(command) {
-		case 'up':
-			command = "$'\e'[A"
-			break;
-		case 'down':
-			command = "$'\e'[B"
-			break;
-		case 'right':
-			command = "$'\e'[C"
-			break;
-		case 'left':
-			command = "$'\e'[D"
-			break;
-		case 'q':
-			exec('pkill -f omxplayer', function(err, stdout, stderr) {
-				res.send("Killed");
-			});
-			return;
-			break;
-	}
+/*END USERS*/
 
 
-	fs.appendFile('FIFO', command, function(err) {
-		if (err) throw err;
-		res.send("Success");
-	})
-})
 
 
-app.post('/del/:typename/:fname', function(req, res) {
-	console.log("Deleting " + req.params.fname)
-	child = exec('rm ./public/assets/' + req.params.typename + '/' + req.params.fname, 
-		function(error, stdout, stderr) {
-			console.log('stdout: ' + stdout);
-			console.log('stderr: ' + stderr);
-			if (stderr !== "")
-				res.send(stderr)
-			else res.send("ok")
+
+/*SHAIRPORT*/
+
+app.get('/shairport/metadata', function(req, res) {
+	http.get('http://10.0.0.90:8081/metadata', function(response) {
+		var body = "";
+		response.on('data', function(chunk) {
+			body += chunk;
 		});
+		response.on('end', function() {
+			res.send(body);
+		});
+	}).on('error', function(err) {
+		if (err.code == 'ECONNREFUSED' | err.code == 'EHOSTUNREACH') {
+			res.send(err.code);
+		} else throw err;
+	})
 })
 
-app.listen(8000, '0.0.0.0');
-console.log("App listening on port 8000");
-fs.appendFile('log.output', 'Time: ' + (new Date()), function(err) {
-	console.log("Recorded")
-});
+/*END SHAIRPORT*/
+
+
+
+
+/*TIMER*/
+var timer = function(callback, time) {
+	console.log("Testing: " + time + " at " + new Date());
+	if (new Date().getTime() > time.getTime()) {
+		callback()
+	} else {
+		setTimeout(function() {
+			timer(callback, time)
+		}, 3000)
+	}
+}
+
+var runTimer = function(date) {
+	timer(function() {
+		console.log("ALARM ALARM ALARM");
+		http.get('http://10.0.0.65:8080/on')
+		http.get('http://10.0.0.12:8080/on0')
+	}, date)
+}
+
+runTimer(new Date(2016, 10, 4, 7, 30, 0));
+
+app.post('/alarm/set', function(req, res) {
+	var post = req.body;
+	runTimer(post.date);
+})
+/*END TIMER*/
 
 
 
 
 
-
-
+var port = 8000;
+app.listen(port, '0.0.0.0');
+console.log("App listening on port " + port);
